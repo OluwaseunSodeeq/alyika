e29f4cf4520083c09d0a6087b71bea6a8c7d4ed1
OluwaseunSodeeq <ademolaoluwaseun90@gmail.com>
2026-03-23 12:39:39 +0100

    About Page updated!

--- app/about/page.js
+++ app/about/page.js
@@ -1,8 +1,11 @@
 import React from "react";
 import AboutHeroSection from "../components/AboutHeroSection";
-import MeetTheTeam from "../components/MeetTheTeam";
-// import AboutCards from "../components/AboutCards";
 import Wrapper from "../components/Wrapper";
+import MeetTheTeamImages from "../components/MeetTheTeamImages";
+
+// Initial Ui ideas
+// import MeetTheTeam from "../components/MeetTheTeam";
+// import AboutCards from "../components/AboutCards";
 
 export default function Page() {
   return (
@@ -10,7 +13,7 @@ export default function Page() {
       <div className="md:px-[1.5rem] xl:px-[3.5rem]">
         <AboutHeroSection />
         {/* <AboutCards /> */}
-        <MeetTheTeam />
+        <MeetTheTeamImages />
       </div>
     </Wrapper>
   );
